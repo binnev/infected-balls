1fdc1d7c0902b22f8487d1ad3538994f51aa5992
Robin Neville <robin@tranzer.com>
2020-03-28 13:08:19 +0100

    Initial commit.

---
+++ conf.py
@@ -0,0 +1,9 @@
+INFECTED = "infected"
+RECOVERED = "recovered"
+HEALTHY = "healthy"
+
+COLORS = {INFECTED: "red", HEALTHY: "cyan", RECOVERED: "pink"}
+
+DEFAULT_SPEED = 0.15
+
+INFECTION_DURATION = 100
---
+++ sandbox.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+# import pandas as pd
+from objects import Ball, Canvas, INFECTED, RECOVERED, PopulationHealth, HEALTHY, COLORS
+
+population_health = PopulationHealth()
+
+canvas = Canvas(balls=[
+    Ball(position=(2, 2), health=HEALTHY),
+])
+population_health.append(canvas.population_health_percentages)
+for _ in range(10):
+    canvas.add_balls(Ball(health=INFECTED))
+    population_health.append(canvas.population_health_percentages)
+
+for _ in range(10):
+    canvas.add_balls(Ball(health=RECOVERED))
+    population_health.append(canvas.population_health_percentages)
+
+
+
+# ================ plots ===================
+percentages = population_health.data
+
+# Make the plot
+labels = [
+    INFECTED,
+    HEALTHY,
+    RECOVERED,
+]
+series = [percentages[label] for label in labels]
+colors = [COLORS[label] for label in labels]
+
+sp = plt.stackplot(range(len(series[0])), *series, labels=labels, colors=colors)
+plt.legend(loc='upper left')
+plt.margins(0, 0)
+plt.title('population health')
+plt.show()
